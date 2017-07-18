d58437c0141ccb6b854d96d441916fe344132f7e
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2017-07-18 14:16:37 +0200

    Callouts implementation

--- src/com/trekglobal/vaadin/ui/IWebFieldView.java
+++ src/com/trekglobal/vaadin/ui/IWebFieldView.java
@@ -4,6 +4,7 @@ import com.trekglobal.vaadin.mobile.MobileLookupGenericObject;
 
 public interface IWebFieldView {
 
+	void onChange(WebField webField);
 	void onLocationLookUp(WebField webField);
 	void onLookUp(WebField webField);
 	void onLookUpOK(WebField webField, MobileLookupGenericObject selectedRecord);
