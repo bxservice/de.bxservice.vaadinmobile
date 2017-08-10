af2765a57fe2f23b8115d252779798add6b7f003
druiz <druiz@localhost>
2017-08-10 18:41:58 +0200

    Implement location fields

--- src/com/trekglobal/vaadin/ui/IWebFieldListener.java
+++ src/com/trekglobal/vaadin/ui/IWebFieldListener.java
@@ -1,11 +1,15 @@
 package com.trekglobal.vaadin.ui;
 
+import org.compiere.model.MLocation;
+
 import com.trekglobal.vaadin.mobile.MobileLookupGenericObject;
 
 public interface IWebFieldListener {
 
 	void onChange(WebField webField);
-	void onLocationLookUp(WebField webField);
+	void onLocation(WebField webField);
+	void onLocation(WebField webField, MLocation location, int m_windowNo);
+	void onLocationOk(WLocationView locationView);
 	void onLookUp(WebField webField);
 	void onLookUpOK(WebField webField, MobileLookupGenericObject selectedRecord);
 	void onLookUpCancel();
