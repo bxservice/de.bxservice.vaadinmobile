6bb41fc4f0434a59bf72c2be78c84a8bf7176cab
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2017-08-14 17:52:55 +0200

    Support reports

---
+++ com.trekglobal.vaadinmobile/src/com/trekglobal/vaadin/ui/OnDemandFileDownloader.java
@@ -0,0 +1,39 @@
+package com.trekglobal.vaadin.ui;
+
+import java.io.IOException;
+
+import com.vaadin.server.FileDownloader;
+import com.vaadin.server.StreamResource;
+import com.vaadin.server.StreamResource.StreamSource;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinResponse;
+
+public class OnDemandFileDownloader extends FileDownloader {
+
+	/**
+	 * Provide both the {@link StreamSource} and the filename in an on-demand way.
+	 */
+	public interface OnDemandStreamResource extends StreamSource {
+		String getFilename();
+	}
+
+	private static final long serialVersionUID = 1L;
+	private final OnDemandStreamResource onDemandStreamResource;
+
+	public OnDemandFileDownloader (OnDemandStreamResource onDemandStreamResource) {
+		super(new StreamResource(onDemandStreamResource, ""));
+		this.onDemandStreamResource = onDemandStreamResource;
+	}
+
+	@Override
+	public boolean handleConnectorRequest (VaadinRequest request, VaadinResponse response, String path)
+			throws IOException {
+		getResource().setFilename(onDemandStreamResource.getFilename());
+		return super.handleConnectorRequest(request, response, path);
+	}
+
+	private StreamResource getResource () {
+		return (StreamResource) this.getResource("dl");
+	}
+
+}
